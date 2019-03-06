86d7d9fa5e32d325ac8471df67e806b2e7af0bb1
adumnici <adriandumnici@yahoo.com>
2019-03-06 17:16:10 +0200

    security improvements

--- controllers/image.js
+++ controllers/image.js
@@ -1,4 +1,19 @@
-handleImage = (req, res, db)=> {
+const Clarifai = require ('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: '669997d47f2f4c7f9e52e0a1ce9601b6'
+   });
+
+const handleApiCall = (req, res) => {
+    app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+        res.json(data)
+    })
+    .catch(err => res.status(400).json('unable to work with the api'))
+}
+
+const handleImage = (req, res, db)=> {
     const {id} = req.body;
    db('users').where('id', '=', id)
    .increment('entries', 1)
@@ -6,9 +21,10 @@ handleImage = (req, res, db)=> {
    .then(entries => {
        res.json(entries[0])
    })
-   .catch(err => res.status(400).json('error getting score'))
+   .catch(err => res.status(400).json('error getting entries'))
 }
 
 module.exports = {
-    handleImage: handleImage
+    handleImage: handleImage,
+    handleApiCall: handleApiCall
 }
