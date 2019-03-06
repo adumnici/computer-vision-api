d384da586381740abd872b9f47002796449138d9
adumnici <adriandumnici@yahoo.com>
2019-03-06 14:49:46 +0200

    improved functionality, database integration (PostgreSQL), a multitude of improvements and changes

---
+++ controllers/image.js
@@ -0,0 +1,14 @@
+handleImage = (req, res, db)=> {
+    const {id} = req.body;
+   db('users').where('id', '=', id)
+   .increment('entries', 1)
+   .returning('entries')
+   .then(entries => {
+       res.json(entries[0])
+   })
+   .catch(err => res.status(400).json('error getting score'))
+}
+
+module.exports = {
+    handleImage: handleImage
+}
